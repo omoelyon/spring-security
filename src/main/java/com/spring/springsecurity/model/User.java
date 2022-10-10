d2650cda3e13fb251b88f33b57f6dab2a544b9a0
Adeola Elegbede <adeola.elegbede@termii.com>
2022-10-10 17:04:18 +0100

    wip

---
+++ src/main/java/com/spring/springsecurity/model/User.java
@@ -0,0 +1,16 @@
+package com.spring.springsecurity.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String authority;
+}
