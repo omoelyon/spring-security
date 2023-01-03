7c69d36f71af97fd2639b46ca4be83cdccaa6855
Adeola Elegbede <adeola.elegbede@termii.com>
2023-01-03 22:19:07 +0100

    security context impl

--- src/main/java/com/spring/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/spring/springsecurity/config/ProjectConfig.java
@@ -1,27 +1,39 @@
 package com.spring.springsecurity.config;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("tade")
-                .password("12345")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
+@EnableAsync
+public class ProjectConfig
+//        extends WebSecurityConfigurerAdapter
+{
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("tade")
+//                .password("12345")
+//                .authorities("read")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic();
+//        http.authorizeRequests()
+//                .anyRequest().authenticated();
+//    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest().authenticated();
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName( SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
