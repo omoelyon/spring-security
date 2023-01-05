517c1e44c1408b30af17c3ff9f853bb4581ea6df
Adeola Elegbede <adeola.elegbede@termii.com>
2023-01-05 23:13:12 +0100

    chapter 5 samples

--- src/main/java/com/spring/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/spring/springsecurity/config/ProjectConfig.java
@@ -1,27 +1,26 @@
 package com.spring.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("tade")
-                .password("12345")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest().authenticated();
+public class ProjectConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.httpBasic(c-> {
+//            c.realmName("OTHER");
+//            c.authenticationEntryPoint(new CustomEntryPoint());
+//        });
+        http.formLogin();
+        http
+                .authorizeRequests().anyRequest().authenticated();
+        return http.build();
     }
 }
--- src/main/java/com/spring/springsecurity/controllers/HelloController.java
+++ src/main/java/com/spring/springsecurity/controllers/HelloController.java
@@ -1,12 +1,13 @@
 package com.spring.springsecurity.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        return "home.html";
     }
 }
