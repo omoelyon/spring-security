cb701c41932d52eca989981e15558dcacac18705
Adeola Elegbede <adeola.elegbede@termii.com>
2023-01-07 04:27:57 +0100

    master

--- src/main/java/com/spring/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/spring/springsecurity/config/ProjectConfig.java
@@ -1,27 +1,7 @@
 package com.spring.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("tade")
-                .password("12345")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
+public class ProjectConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest().authenticated();
-    }
 }
